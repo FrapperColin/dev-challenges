7b864ffe938809797180c4efa7d11a384c25a1d8
cfrapper <colin.frapper@gmail.com>
2020-11-26 20:46:44 +0100

    typo onclick

--- windbnb-master/windbnb-master/windbnb/src/components/SearchStays/SearchStaysContainer.js
+++ windbnb-master/windbnb-master/windbnb/src/components/SearchStays/SearchStaysContainer.js
@@ -7,7 +7,7 @@ const SearchStaysContainer = props => {
     return (
         <div className={`${isExpanded ? "search-stay-expanded" : "search-stay"}`}>
             <span className="search-stay-location" onClick={() => expandDropdown("location")}>Helsinki, Findland</span>
-            <span className="search-stay-guest" nClick={() => expandDropdown("guest")}>Add guest</span>
+            <span className="search-stay-guest" onClick={() => expandDropdown("guest")}>Add guest</span>
             <span><i className="material-icons search-icon">search</i></span> 
         </div>
     );
